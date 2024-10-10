efa49af127a0d7d1554adbf03d950ab149570076
yvv4git <yvv4m@mail.ru>
2024-10-10 22:09:41 +0300

    Add tls secure connetion

--- internal/infrastructure/tcp_client.go
+++ internal/infrastructure/tcp_client.go
@@ -2,8 +2,11 @@ package infrastructure
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net"
 
 	"github.com/songgao/water"
@@ -23,16 +26,9 @@ func NewClientTCP(cfg Client, tunDevice *water.Interface) *ClientTCP {
 }
 
 func (c *ClientTCP) Start(ctx context.Context) error {
-	addr, err := createAddrString(c.cfg.ServerHost, c.cfg.ServerPort)
-	if err != nil {
-		return fmt.Errorf("create client TCP address: %w", err)
-	}
-
-	conn, err := net.Dial(tcpProtocol, addr)
-	if err != nil {
+	if err := c.setupConn(); err != nil {
 		return fmt.Errorf("create client TCP connection: %w", err)
 	}
-	c.conn = conn
 
 	go c.tunToTCP(ctx)
 	go c.tcpToTun(ctx)
@@ -41,8 +37,53 @@ func (c *ClientTCP) Start(ctx context.Context) error {
 	return ctx.Err()
 }
 
+func (c *ClientTCP) setupConn() error {
+	clientCfg := c.cfg.TCPConfig
+	addr, err := createAddrString(clientCfg.ServerHost, clientCfg.ServerPort)
+	if err != nil {
+		return fmt.Errorf("create client TCP address: %w", err)
+	}
+
+	if clientCfg.Encryption.Enabled {
+		encCfg := clientCfg.Encryption
+		cer, err := tls.LoadX509KeyPair(encCfg.ClientCert, encCfg.ClientKey)
+		if err != nil {
+			return fmt.Errorf("load client certificates: %w", err)
+		}
+
+		caCert, err := ioutil.ReadFile(encCfg.CACert)
+		if err != nil {
+			return fmt.Errorf("read CA certificate: %w", err)
+		}
+
+		caCertPool := x509.NewCertPool()
+		caCertPool.AppendCertsFromPEM(caCert)
+
+		config := &tls.Config{
+			Certificates: []tls.Certificate{cer},
+			RootCAs:      caCertPool,
+		}
+
+		conn, err := tls.Dial(tcpProtocol, addr, config)
+		if err != nil {
+			return fmt.Errorf("create client TLS connection: %w", err)
+		}
+		c.conn = conn // setup tls conn
+
+		return nil
+	}
+
+	conn, err := net.Dial(tcpProtocol, addr)
+	if err != nil {
+		return fmt.Errorf("create client TCP connection: %w", err)
+	}
+	c.conn = conn // setup plain conn
+
+	return nil
+}
+
 func (c *ClientTCP) tunToTCP(ctx context.Context) {
-	buffer := make([]byte, c.cfg.BufferSize)
+	buffer := make([]byte, c.cfg.TCPConfig.BufferSize)
 
 	for {
 		select {
@@ -68,7 +109,7 @@ func (c *ClientTCP) tunToTCP(ctx context.Context) {
 }
 
 func (c *ClientTCP) tcpToTun(ctx context.Context) {
-	buffer := make([]byte, c.cfg.BufferSize)
+	buffer := make([]byte, c.cfg.TCPConfig.BufferSize)
 
 	for {
 		select {
