da450b77a4c1c8a19307e6b6fb55f47ccc2c7803
yvv4git <yvv4m@mail.ru>
2024-10-20 21:02:56 +0300

    Update script for running linux server

--- scripts/remote_server/run_linux_server.sh
+++ scripts/remote_server/run_linux_server.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 docker run -d \
-  --rm \
   --name server \
   --hostname server \
   -p 1234:1234 \
@@ -9,5 +8,5 @@ docker run -d \
   --device /dev/net/tun \
   -v $(pwd)/configs/config.server.toml:/app/configs/config.toml \
   -v $(pwd)/configs/encryption/certs/:/app/configs/encryption/certs/ \
-  yvv4docker/tunnel-linux \
-  ./tunnel server
+  yvv4docker/tunnel-linux:v0.0.1 \
+  ./tunnel server --config ./configs/config.toml
