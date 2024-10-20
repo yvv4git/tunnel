9106bd150f56722c7ad213c9336c86f75df32a3a
yvv4git <yvv4m@mail.ru>
2024-10-20 21:01:55 +0300

    Fix client application config init

--- internal/application/client.go
+++ internal/application/client.go
@@ -35,14 +35,14 @@ func (c *Client) start(ctx context.Context) error {
 		return fmt.Errorf("create TUN device: %w", err)
 	}
 
-	tunDevice, err := tunDeviceBuilder.Build(infrastructure.Platform(c.cfg.Server.DeviceTUN.Platform))
+	tunDevice, err := tunDeviceBuilder.Build(infrastructure.Platform(c.cfg.Client.DeviceTUN.Platform))
 	if err != nil {
 		return fmt.Errorf("build TUN device: %w", err)
 	}
 	defer tunDevice.Close()
 
 	channelClientBuilder := infrastructure.NewChannelClientBuilder(c.cfg, tunDevice)
-	channelClient, err := channelClientBuilder.Build(c.cfg.Server.ChannelType)
+	channelClient, err := channelClientBuilder.Build(c.cfg.Client.ChannelType)
 	if err != nil {
 		return fmt.Errorf("build client: %w", err)
 	}
