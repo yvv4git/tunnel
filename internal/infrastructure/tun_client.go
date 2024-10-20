c7b0956910f141599cfbfbfcaaf0827920801570
yvv4git <yvv4m@mail.ru>
2024-10-20 21:06:19 +0300

    Refactoring tun client

--- internal/infrastructure/tun_client.go
+++ internal/infrastructure/tun_client.go
@@ -29,10 +29,14 @@ func NewDeviceTUNClientBuilder(cfg Config) (*DeviceTUNClientBuilder, error) {
 func (t *DeviceTUNClientBuilder) Build(platform Platform) (*water.Interface, error) {
 	switch platform {
 	case PlatformLinux:
-		t.configureClientForLinux()
+		if err := t.configureClientForLinux(); err != nil {
+			return t.iface, err
+		}
 		return t.iface, nil
 	case PlatformMacOC:
-		t.configureClientForMacOS()
+		if err := t.configureClientForMacOS(); err != nil {
+			return t.iface, err
+		}
 		return t.iface, nil
 	}
 
@@ -60,6 +64,21 @@ func (t *DeviceTUNClientBuilder) configureClientForLinux() error {
 }
 
 func (t *DeviceTUNClientBuilder) configureClientForMacOS() error {
-	// TODO: implement
+	cfgDeviceTUN := t.cfg.Client.DeviceTUN
+	cfgServerTUN := t.cfg.Server.DeviceTUN
+
+	// Assign IP address to the interface and bring it up
+	cmd := exec.Command("sudo", "ifconfig", t.iface.Name(), cfgDeviceTUN.Host, cfgServerTUN.Host, "up")
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("assign IP address and bring up TUN device: %w", err)
+	}
+	fmt.Println("TUN device is up: ", t.iface.Name())
+
+	// Add route to the interface
+	cmd = exec.Command("sudo", "route", "add", "-net", cfgDeviceTUN.Route, "-interface", t.iface.Name())
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("add route to TUN device: %w", err)
+	}
+
 	return nil
 }
